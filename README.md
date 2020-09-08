5c40a531a001984aba684ef428f24f0a96521cc8
fitzpeng <821397569@qq.com>
2020-09-09 00:01:27 +0800

    912. 排序数组(堆排)

--- README.md
+++ README.md
@@ -102,3 +102,60 @@ public:
     }
 };
 ```
+## 912. 排序数组(堆排序)
+```
+class Solution {
+public:
+    void push(int val) {
+        heap[++top] = val;
+        int temp = top;
+        while (temp > 1) {
+            int root = temp / 2;
+            if (heap[root] > val) swap(heap[root], heap[temp]);
+            else break;
+            temp = root;
+        }
+    }
+
+    int pop() {
+        if (top == 0) return -999999;
+        int ret = heap[1];
+        heap[1] = heap[top];
+        int root = 1;
+        while (2 * root < top) {
+            int L = root * 2;
+            int R = root * 2 + 1;
+            if (R >= top || heap[L] < heap[R]) {
+                if (heap[root] > heap[L]) {
+                    swap(heap[root], heap[L]);
+                    root = L;
+                } else break;
+            } else {
+                if (heap[root] > heap[R]) {
+                    swap(heap[root], heap[R]);
+                    root = R;
+                } else break;
+            }
+        }
+        top--;
+        return ret;
+    }
+
+    vector<int> sortArray(vector<int> &nums) {
+        top = 0;
+        vector<int> ans;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            push(nums[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            ans.push_back(pop());
+        }
+        return ans;
+    }
+
+private:
+    int top, heap[100010];
+};
+
+```
--- main.cpp
+++ main.cpp
@@ -1,25 +1,60 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+const int maxn = 1e5 + 7;
 
 class Solution {
 public:
-    int firstMissingPositive(vector<int> &nums) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0) nums[i] = n + 1;
+    void push(int val) {
+        heap[++top] = val;
+        int temp = top;
+        while (temp > 1) {
+            int root = temp / 2;
+            if (heap[root] > val) swap(heap[root], heap[temp]);
+            else break;
+            temp = root;
         }
-        for (int i = 0; i < n; i++) {
-            int num = abs(nums[i]);
-            if (num<= n) {
-                nums[num - 1] = -abs(nums[num - 1]);
+    }
+
+    int pop() {
+        if (top == 0) return -999999;
+        int ret = heap[1];
+        heap[1] = heap[top];
+        int root = 1;
+        while (2 * root < top) {
+            int L = root * 2;
+            int R = root * 2 + 1;
+            if (R >= top || heap[L] < heap[R]) {
+                if (heap[root] > heap[L]) {
+                    swap(heap[root], heap[L]);
+                    root = L;
+                } else break;
+            } else {
+                if (heap[root] > heap[R]) {
+                    swap(heap[root], heap[R]);
+                    root = R;
+                } else break;
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) return i + 1;
-        }
-        return n + 1;
+        top--;
+        return ret;
     }
+
+    vector<int> sortArray(vector<int> &nums) {
+        top = 0;
+        vector<int> ans;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            push(nums[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            ans.push_back(pop());
+        }
+        return ans;
+    }
+
+private:
+    int top, heap[10010];
 };
 
 int main() {
