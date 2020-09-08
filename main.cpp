2ed73eb77fde02b3bc477c1bb5188f3723e62c37
fitzpeng <821397569@qq.com>
2020-09-08 23:38:09 +0800

    41. 缺失的第一个正数

--- main.cpp
+++ main.cpp
@@ -4,30 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>> &matrix, int k) {
-        int n = matrix.size();
-        if (n == 0) return false;
-        int m = matrix[0].size();
-        priority_queue<int> que;
-        while (!que.empty())que.pop();
+    int firstMissingPositive(vector<int> &nums) {
+        int n = nums.size();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (que.size() < k) {
-                    que.push(matrix[i][j]);
-                } else {
-                    que.push(matrix[i][j]);
-                    que.pop();
-                }
+            if (nums[i] <= 0) nums[i] = n + 1;
+        }
+        for (int i = 0; i < n; i++) {
+            int num = abs(nums[i]);
+            if (num<= n) {
+                nums[num - 1] = -abs(nums[num - 1]);
             }
         }
-        return que.top();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) return i + 1;
+        }
+        return n + 1;
     }
 };
 
 int main() {
-    priority_queue<int> que;
-    que.push(1);
-    que.push(3);
-    printf("%d\n",que.top());
+
     return 0;
 }
