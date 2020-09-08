6bfda03a54c3bc4bb7283488d030cd08a87d743f
fitzpeng <821397569@qq.com>
2020-09-09 00:15:03 +0800

    415. 字符串相加

--- README.md
+++ README.md
@@ -157,5 +157,45 @@ public:
 private:
     int top, heap[100010];
 };
+```
 
+## 415. 字符串相加
+```
+class Solution {
+public:
+    int num[100010];
+
+    string addStrings(string num1, string num2) {
+        int n = num1.size(), m = num2.size();
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        int cy = 0;
+        for (int i = 0; i < min(n, m); i++) {
+            int val = num1[i] - '0' + num2[i] - '0' + cy;
+            num[i] = val % 10;
+            cy = val / 10;
+        }
+        if (n >= m) {
+            for (int i = min(n, m); i < max(n, m); i++) {
+                int val = num1[i] - '0' + cy;
+                num[i] = val % 10;
+                cy = val / 10;
+            }
+        } else {
+            for (int i = min(n, m); i < max(n, m); i++) {
+                int val = num2[i] - '0' + cy;
+                num[i] = val % 10;
+                cy = val / 10;
+            }
+        }
+        int len = max(n, m);
+        if (cy) num[max(n, m)] = cy, len++;
+        string ans;
+        for (int i = 0; i < len; i++) {
+            ans += std::to_string(num[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
 ```
--- main.cpp
+++ main.cpp
@@ -5,56 +5,40 @@ const int maxn = 1e5 + 7;
 
 class Solution {
 public:
-    void push(int val) {
-        heap[++top] = val;
-        int temp = top;
-        while (temp > 1) {
-            int root = temp / 2;
-            if (heap[root] > val) swap(heap[root], heap[temp]);
-            else break;
-            temp = root;
-        }
-    }
+    int num[100010];
 
-    int pop() {
-        if (top == 0) return -999999;
-        int ret = heap[1];
-        heap[1] = heap[top];
-        int root = 1;
-        while (2 * root < top) {
-            int L = root * 2;
-            int R = root * 2 + 1;
-            if (R >= top || heap[L] < heap[R]) {
-                if (heap[root] > heap[L]) {
-                    swap(heap[root], heap[L]);
-                    root = L;
-                } else break;
-            } else {
-                if (heap[root] > heap[R]) {
-                    swap(heap[root], heap[R]);
-                    root = R;
-                } else break;
+    string addStrings(string num1, string num2) {
+        int n = num1.size(), m = num2.size();
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        int cy = 0;
+        for (int i = 0; i < min(n, m); i++) {
+            int val = num1[i] - '0' + num2[i] - '0' + cy;
+            num[i] = val % 10;
+            cy = val / 10;
+        }
+        if (n >= m) {
+            for (int i = min(n, m); i < max(n, m); i++) {
+                int val = num1[i] - '0' + cy;
+                num[i] = val % 10;
+                cy = val / 10;
+            }
+        } else {
+            for (int i = min(n, m); i < max(n, m); i++) {
+                int val = num2[i] - '0' + cy;
+                num[i] = val % 10;
+                cy = val / 10;
             }
         }
-        top--;
-        return ret;
-    }
-
-    vector<int> sortArray(vector<int> &nums) {
-        top = 0;
-        vector<int> ans;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            push(nums[i]);
+        int len = max(n, m);
+        if (cy) num[max(n, m)] = cy, len++;
+        string ans;
+        for (int i = 0; i < len; i++) {
+            ans += std::to_string(num[i]);
         }
-        for (int i = 0; i < n; i++) {
-            ans.push_back(pop());
-        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
-
-private:
-    int top, heap[10010];
 };
 
 int main() {
