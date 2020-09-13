1fd87ca0763279e6d75b513895358f4587f3f22e
fitzpeng <821397569@qq.com>
2020-09-13 21:51:06 +0800

    ## 543 二叉树的直径

--- README.md
+++ README.md
@@ -352,3 +352,25 @@ public:
     }
 } solve;
 ```
+## 543 二叉树的直径
+
+```
+class Solution {
+public:
+    int ans;
+
+    int depth(TreeNode *root) {
+        if (root == NULL) return 0;
+        int L = depth(root->left);
+        int R = depth(root->right);
+        ans = max(ans, L + R + 1);
+        return max(L, R) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode *root) {
+        ans = 1;
+        depth(root);
+        return ans-1;
+    }
+};
+```
--- main.cpp
+++ main.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int ans;
+
+    int depth(TreeNode *root) {
+        if (root == NULL) return 0;
+        int L = depth(root->left);
+        int R = depth(root->right);
+        ans = max(ans, L + R + 1);
+        return max(L, R) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode *root) {
+        ans = 1;
+        depth(root);
+        return ans-1;
+    }
+};
 
 int main() {
-return 0;
+    return 0;
 }
