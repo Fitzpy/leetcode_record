a006559ed506536eafcb7d305731be0cefe4b983
fitzpeng <821397569@qq.com>
2020-09-15 20:15:08 +0800

    美团2面

--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+int n;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution {
-public:
-    int maxSubArray(vector<int> &nums) {
-        int ans = nums[0], pre = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            pre = max(pre+nums[i],nums[i]);
-            ans = max(ans,pre);
-        }
-        return ans;
+void dfs(int pos, int num1, int num2, string s) {
+    if (pos == 2 * n) {
+        cout << s << endl;
+        return;
     }
-};
-
+    if (num1 + 1 <= n && num1 + 1 >= num2) {
+        dfs(pos + 1, num1 + 1, num2, s + '0');
+    }
+    if (num2 + 1 <= n && num1 >= num2 + 1) {
+        dfs(pos + 1, num1, num2 + 1, s + '1');
+    }
+}
+unordered_map<>
 int main() {
-
+    scanf("%d", &n);
+    dfs(0, 0, 0, "");
     return 0;
 }
