9cf969f342e13b9f8ec2628f96ad2b17ae2f06ac
fitzpeng <821397569@qq.com>
2020-09-21 16:52:35 +0800

    ## 手写share_ptr

--- main.cpp
+++ main.cpp
@@ -9,24 +9,72 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution {
-public:
-    ListNode *getKthFromEnd(ListNode *head, int k) {
-        ListNode *now1 = head, *now2 = head;
-        int num = 0;
-        while (now2 != NULL) {
-            num++;
-            if (num < k) {
-                now2 = now2->next;
-            } else {
-                now1 = now1->next;
-                now2 = now2->next;
-            }
-        }
-        return now1;
+template<typename T>
+class SmartPtr;
+
+template<typename T>
+class U_Ptr {
+private:
+    friend class SmartPtr<T>;
+
+    U_Ptr(T *ptr) : p(ptr), count(1) {}
+
+    ~U_Ptr() {
+        delete p;
     }
+
+    T *p;
+    atomic<int> count;
+};
+
+template<typename T>
+class SmartPtr {
+public:
+    SmartPtr(T *ptr) : rp(new U_Ptr<T>(ptr)) {}
+
+    SmartPtr(const SmartPtr<T> &sp) : rp(sp.rp) {
+        ++rp->count;
+    }
+
+    SmartPtr &operator=(const SmartPtr<T> &rhs) {
+        ++rhs.rp->count;
+        if (--rp->count == 0) delete rp;
+        rp = rhs.rp;
+        return *this;
+    }
+
+    T &operator*() {
+        return *(rp->p);
+    }
+
+    T *operator->() {
+        return rp->p;
+    }
+
+    ~SmartPtr() {
+        if (--rp->count == 0) {
+            delete rp;
+        } else cout << "还有" << rp->count << "个指针指向基础对象" << endl;
+    }
+
+private:
+    U_Ptr<T> *rp;
 };
 
 int main() {
+    int *i = new int(2);
+    {
+        SmartPtr<int> ptr1(i);
+        {
+            SmartPtr<int> ptr2(ptr1);
+            {
+                SmartPtr<int> ptr3 = ptr2;
+                cout << *ptr1 << endl;
+                *ptr1 = 20;
+                cout << *ptr2 << endl;
+            }
+        }
+    }
+    system("pause");
     return 0;
 }
