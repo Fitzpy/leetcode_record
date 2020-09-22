6a743d49ca1c858586ac74c7e09169fab6b94269
fitzpeng <821397569@qq.com>
2020-09-22 23:26:44 +0800

    337. 打家劫舍 III

--- README.md
+++ README.md
@@ -1101,3 +1101,30 @@ void HashMap::rehash() {
 }
 
 ```
+
+## 337. 打家劫舍 III
+```
+class Solution {
+public:
+    unordered_map<TreeNode *, int> mp1, mp2;
+
+    void dfs(TreeNode *root) {
+        if (root == NULL) {
+            return;
+        }
+        dfs(root->left);
+        dfs(root->right);
+        mp1[root] = root->val + mp2[root->left] + mp2[root->right];
+        mp2[root] = max(mp1[root->left], mp2[root->left]) + max(mp1[root->right], mp2[root->right]);
+
+    }
+
+    int rob(TreeNode *root) {
+        mp1.clear();
+        mp2.clear();
+        dfs(root);
+        return max(mp1[root], mp2[root]);
+    }
+};
+
+```
--- main.cpp
+++ main.cpp
@@ -9,72 +9,38 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class HashMap {
-public:
-    HashMap() {
-        capacity = 10;
-        mp.resize(capacity);
-    }
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
 
-    void put(unsigned long long key, int val);
-
-    int get(unsigned long long key);
-
-    void rehash();
-
-private:
-    vector<list<pair<unsigned long long, int>>> mp;
-    int capacity;
-    int size = 0;
-    unsigned long long seed = 13331;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-void HashMap::put(unsigned long long key, int val) {
-    int idx = key % capacity;
-    for (auto it = mp[idx].begin(); it != mp[idx].end(); it++) {
-        if (it->first == key) {
-            it->second = val;
+class Solution {
+public:
+    unordered_map<TreeNode *, int> mp1, mp2;
+
+    void dfs(TreeNode *root) {
+        if (root == NULL) {
             return;
         }
-    }
-    size++;
-    mp[idx].push_back(make_pair(key, val));
-    if (size == capacity) rehash();
-}
+        dfs(root->left);
+        dfs(root->right);
+        mp1[root] = root->val + mp2[root->left] + mp2[root->right];
+        mp2[root] = max(mp1[root->left], mp2[root->left]) + max(mp1[root->right], mp2[root->right]);
 
-int HashMap::get(unsigned long long key) {
-    int idx = key % capacity;
-    for (auto it = mp[idx].begin(); it != mp[idx].end(); it++) {
-        if (it->first == key) {
-            return it->second;
-        }
     }
-    return -1;
-}
 
-void HashMap::rehash() {
-    capacity *= 2;
-    mp.resize(capacity);
-    for (int i = 0; i < capacity / 2; i++) {
-        for (auto it = mp[i].begin(); it != mp[i].end(); it++) {
-            unsigned long long key = it->first;
-            int val = it->second;
-            int idx = key % (2 * capacity);
-            if (idx == i) continue;
-            put(key, val);
-        }
+    int rob(TreeNode *root) {
+        mp1.clear();
+        mp2.clear();
+        dfs(root);
+        return max(mp1[root], mp2[root]);
     }
-}
+};
 
 int main() {
-    HashMap mp;
-    mp.put(123, 1);
-    mp.put(123123, 2);
-    mp.put(111, 3);
-    mp.put(222, 4);
-    cout << mp.get(123) << endl;
-    cout << mp.get(123123) << endl;
-    cout << mp.get(111) << endl;
-    cout << mp.get(222) << endl;
+
     return 0;
 }
