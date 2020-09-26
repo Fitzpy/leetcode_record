12e680499cf3338b9e6af611617ed7518bc9d14b
fitzpeng <821397569@qq.com>
2020-09-26 15:43:48 +0800

    百度面经

--- main.cpp
+++ main.cpp
@@ -2,20 +2,6 @@
 
 using namespace std;
 
-class A {
-public:
-    A(int x = 0) {
-        a = x;
-    }
-
-    void display() {
-        printf("%d\n", a);
-    }
-
-private:
-    int a;
-};
-
 int main() {
 
     return 0;
