cbf7a0dc5d57a7d7df0ad2ae86be5d6a83c79b4b
fitzpeng <821397569@qq.com>
2020-09-28 21:19:43 +0800

    ## 依图一面

--- main.cpp
+++ main.cpp
@@ -2,45 +2,14 @@
 
 using namespace std;
 
-
-class Node {
+template<typename T>
+class My_Unique_ptr {
 public:
-    int val;
-    Node *next;
-    Node *random;
+    My_Unique_ptr(const T *ptr) : p(ptr) {};
 
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
+private:
 
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> st;
-        int len = s.size();
-        for (int i = 0; i < len; i++) {
-            if (st.empty() || s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
-            else {
-                if (s[i] == ')') {
-                    if (st.top() != '(') return false;
-                    else st.pop();
-                }
-                if (s[i] == '}') {
-                    if (st.top() != '{') return false;
-                    else st.pop();
-                }
-                if (s[i] == ']') {
-                    if (st.top() != '[') return false;
-                    else st.pop();
-                }
-            }
-        }
-        if (st.empty()) return true;
-        return false;
-    }
+    T *p;
 };
 
 int main() {
