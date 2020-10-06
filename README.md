8f21e91b9c71017d2dfa755a2e3814a449836fdc
fitzpeng <821397569@qq.com>
2020-10-06 18:53:29 +0800

    剑指 Offer 04. 二维数组中的查找

--- README.md
+++ README.md
@@ -1484,3 +1484,26 @@ public:
     }
 };
 ```
+## 剑指 Offer 04. 二维数组中的查找
+```
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>> &matrix, int target) {
+        int n = matrix.size();
+        if (n == 0) return false;
+        int m = matrix[0].size();
+        if (m == 0) return false;
+        int x = 0, y = m - 1;
+        while (x < n && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] > target) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+};
+```
--- main.cpp
+++ main.cpp
@@ -28,37 +28,26 @@ public:
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        string cur;
-        cur += "$#";
-        for (int i = 0; i < s.size(); i++) {
-            cur += s[i];
-            cur += '#';
-        }
-        vector<int> mp(cur.size() + 1, 0);
-        int maxlen = 0, mx = 0, id = 0, ans = 0;
-        for (int i = 1; i < cur.size(); i++) {
-            mp[i] = mx > i ? min(mp[2 * id - i], mx - i) : 1;
-            while (cur[i + mp[i]] == cur[i - mp[i]]) mp[i]++;
-            if (mp[i] + i > mx) {
-                mx = mp[i] + i;
-                id = i;
-            }
-            if (mp[i] - 1 > maxlen) {
-                maxlen = mp[i] - 1;
-                ans = i;
+    bool findNumberIn2DArray(vector<vector<int>> &matrix, int target) {
+        int n = matrix.size();
+        if (n == 0) return false;
+        int m = matrix[0].size();
+        if (m == 0) return false;
+        int x = 0, y = m - 1;
+        while (x < n && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] > target) {
+                y--;
+            } else {
+                x++;
             }
         }
-        string ret;
-        for (int i = ans - mp[ans]+1; i < ans + mp[ans]; i++) {
-            if (cur[i] != '#' && cur[i] !='$')  ret += cur[i];
-        }
-        return ret;
+        return false;
     }
 };
 
 int main() {
-    string s="babad";
-    solve.longestPalindrome(s);
+
     return 0;
 }
