9c07fa33755826e45bb1f069ab7e2c5a713ab0e0
fitzpeng <821397569@qq.com>
2020-10-06 10:22:54 +0800

    647. 回文子串

--- README.md
+++ README.md
@@ -1419,3 +1419,34 @@ public:
 };
 
 ```
+
+## 647. 回文子串
+```
+class Solution {
+public:
+    int countSubstrings(string s) {
+        string cur;
+        cur += '$';
+        cur += '#';
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            cur += s[i];
+            cur += '#';
+        }
+        vector<int> mp(cur.size() + 5, 0);
+        int maxlen = 0, res = 0, mx = 0, id = 0;
+        for (int i = 1; i < cur.size(); i++) {
+            mp[i] = mx > i ? min(mp[2 * id - i], mx - i) : 1;
+            while (cur[i + mp[i]] == cur[i - mp[i]]) mp[i]++;
+            if (i + mp[i] > mx) {
+                mx = i + mp[i];
+                id = i;
+            }
+            maxlen = max(maxlen, mp[i] - 1);
+            res += mp[i] / 2;
+        }
+        cout<<res<<endl;
+        return res;
+    }
+}solve;
+```
--- main.cpp
+++ main.cpp
@@ -13,27 +13,49 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class Node {
 public:
-    void hanota(vector<int> &A, vector<int> &B, vector<int> &C) {
-        int n = A.size();
-        solve(n, A, B, C);
-    }
+    int val;
+    Node *next;
+    Node *random;
 
-    void solve(int n, vector<int> &A, vector<int> &B, vector<int> &C) {
-        if (n == 1) {
-            C.push_back(A.back());
-            A.pop_back();
-            return;
-        }
-        solve(n - 1, A, C, B);
-        C.push_back(A.back());
-        A.pop_back();
-        solve(n - 1, B, A, C);
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
 };
 
-int main() {
+class Solution {
+public:
+    int countSubstrings(string s) {
+        string cur;
+        cur += '$';
+        cur += '#';
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            cur += s[i];
+            cur += '#';
+        }
+        vector<int> mp(cur.size() + 5, 0);
+        int maxlen = 0, res = 0, mx = 0, id = 0;
+        for (int i = 0; i < cur.size(); i++) {
+            mp[i] = mx >= i ? min(mp[2 * id - i], mx - i) : 1;
+            while (cur[i + mp[i]] == cur[i - mp[i]]) mp[i]++;
+            if (i + mp[i] > mx) {
+                mx = i + mp[i];
+                id = i;
+            }
+            maxlen = max(maxlen, mp[i] - 1);
+            res += mp[i] / 2;
+        }
+        cout<<res<<endl;
+        return res;
+    }
+}solve;
 
+int main() {
+    string s = "abc";
+    solve.countSubstrings(s);
     return 0;
 }
