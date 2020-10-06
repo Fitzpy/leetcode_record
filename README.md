01da48773bce7c6a7b23de1dc7944d0930becc68
fitzpeng <821397569@qq.com>
2020-10-06 18:58:18 +0800

    74. 搜索二维矩阵

--- README.md
+++ README.md
@@ -1507,3 +1507,29 @@ public:
     }
 };
 ```
+## 74. 搜索二维矩阵
+```
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        int n = matrix.size();
+        if (n == 0) return false;
+        int m = matrix[0].size();
+        if (m == 0) return false;
+        int low = 0, high = n * m - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int x = mid / m, y = mid % m;
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
+
+```
--- main.cpp
+++ main.cpp
@@ -28,19 +28,21 @@ public:
 
 class Solution {
 public:
-    bool findNumberIn2DArray(vector<vector<int>> &matrix, int target) {
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
         int n = matrix.size();
         if (n == 0) return false;
         int m = matrix[0].size();
         if (m == 0) return false;
-        int x = 0, y = m - 1;
-        while (x < n && y >= 0) {
+        int low = 0, high = n * m - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int x = mid / m, y = mid % m;
             if (matrix[x][y] == target) {
                 return true;
-            } else if (matrix[x][y] > target) {
-                y--;
+            } else if (matrix[x][y] < target) {
+                low = mid + 1;
             } else {
-                x++;
+                high = mid - 1;
             }
         }
         return false;
