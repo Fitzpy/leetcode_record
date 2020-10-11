5d8e92cbb047cb20dc4edaf508f1331780d4a4bc
fitzpeng <821397569@qq.com>
2020-10-11 20:36:56 +0800

    1209. 删除字符串中的所有相邻重复项 II

--- README.md
+++ README.md
@@ -1320,7 +1320,7 @@ char *MyStrCpy(char *dst, char *src) {
     } else {
         while ((*pdst++ = *psrc++) != '\0');
     }
-    return pdst;
+    return dst;
 }
 ```
 ## 手写MemCpy
@@ -1810,6 +1810,36 @@ public:
             }
         }
         return root;
+    }
+};
+```
+## 1209. 删除字符串中的所有相邻重复项 II
+```
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        int n = s.size();
+        stack<pair<char, int>> st;
+        for (int i = 0; i < n; i++) {
+            if (st.empty()) {
+                st.push(make_pair(s[i], 1));
+            } else {
+                if (st.top().first == s[i]) {
+                    if (st.top().second == k - 1) {
+                        while (!st.empty() && st.top().first == s[i]) st.pop();
+                    } else st.push(make_pair(s[i], st.top().second + 1));
+                } else {
+                    st.push(make_pair(s[i], 1));
+                }
+            }
+        }
+        string ret;
+        while (!st.empty()) {
+            ret += st.top().first;
+            st.pop();
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
     }
 };
 ```
--- main.cpp
+++ main.cpp
@@ -2,38 +2,35 @@
 
 using namespace std;
 
-int a[] = {2, 1, 4, 1, 1};
-
-void merge(int L1, int R1, int L2, int R2) {
-    int temp[R2 - L1 + 1];
-    int i = L1, j = L2, pos = 0;
-    while (i <= R1 && j <= R2) {
-        if (a[i] <= a[j]) {
-            temp[pos++] = a[i];
-            i++;
-        } else {
-            temp[pos++] = a[j];
-            j++;
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        int n = s.size();
+        stack<pair<char, int>> st;
+        for (int i = 0; i < n; i++) {
+            if (st.empty()) {
+                st.push(make_pair(s[i], 1));
+            } else {
+                if (st.top().first == s[i]) {
+                    if (st.top().second == k - 1) {
+                        while (!st.empty() && st.top().first == s[i]) st.pop();
+                    } else st.push(make_pair(s[i], st.top().second + 1));
+                } else {
+                    st.push(make_pair(s[i], 1));
+                }
+            }
         }
+        string ret;
+        while (!st.empty()) {
+            ret += st.top().first;
+            st.pop();
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
     }
-    while (i <= R1) temp[pos++] = a[i++];
-    while (j <= R2) temp[pos++] = a[j++];
-    for (i = L1; i <= R2; i++) {
-        a[i] = temp[i - L1];
-    }
-}
-
-void mergeSort(int L, int R) {
-    if (L >= R) return;
-    int mid = (L + R) / 2;
-    mergeSort(L, mid);
-    mergeSort(mid + 1, R);
-    merge(L, mid, mid + 1, R);
-}
-
+};
 
 int main() {
-    mergeSort(0, 4);
-    for (int i = 0; i < 5; i++) printf("%d ", a[i]);
+
     return 0;
 }
