1d398eeb23358ff7f8a53096c3250253ae78d076
fitzpeng <821397569@qq.com>
2020-10-11 22:50:34 +0800

    889. 根据前序和后序遍历构造二叉树

--- README.md
+++ README.md
@@ -1893,3 +1893,27 @@ public:
     }
 };
 ```
+## 889. 根据前序和后序遍历构造二叉树
+```
+class Solution {
+public:
+    unordered_map<int, int> mp;
+
+    TreeNode *solve(vector<int> &pre, vector<int> &post, int L1, int R1, int L2, int R2) {
+        if (L1 > R1 || L2 > R2) return NULL;
+        TreeNode *head = new TreeNode(pre[L1]);
+        if (L1 == R1) return head;
+        int pos = mp[pre[L1 + 1]];
+        int len = pos - L2 + 1;
+        head->left = solve(pre, post, L1 + 1, L1 + len, L2, pos);
+        head->right = solve(pre, post, L1 + len + 1, R1, pos + 1, R2 - 1);
+        return head;
+    }
+
+    TreeNode *constructFromPrePost(vector<int> &pre, vector<int> &post) {
+        int n = pre.size();
+        for (int i = 0; i < n; i++) mp[post[i]] = i;
+        return solve(pre, post, 0, n - 1, 0, n - 1);
+    }
+};
+```
--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include <cstdio>
-#include <mutex>
-#include <thread>
-#include <condition_variable>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -15,12 +12,23 @@ struct TreeNode {
 
 class Solution {
 public:
-    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-        if (root == NULL) return root;
-        if (root == p || root == q) return root;
-        TreeNode *left = lowestCommonAncestor(root->left, p, q);
-        TreeNode *right = lowestCommonAncestor(root->right, p, q);
-        return left ? left : right;
+    unordered_map<int, int> mp;
+
+    TreeNode *solve(vector<int> &pre, vector<int> &post, int L1, int R1, int L2, int R2) {
+        if (L1 > R1 || L2 > R2) return NULL;
+        TreeNode *head = new TreeNode(pre[L1]);
+        if (L1 == R1) return head;
+        int pos = mp[pre[L1 + 1]];
+        int len = pos - L2 + 1;
+        head->left = solve(pre, post, L1 + 1, L1 + len, L2, pos);
+        head->right = solve(pre, post, L1 + len + 1, R1, pos + 1, R2 - 1);
+        return head;
+    }
+
+    TreeNode *constructFromPrePost(vector<int> &pre, vector<int> &post) {
+        int n = pre.size();
+        for (int i = 0; i < n; i++) mp[post[i]] = i;
+        return solve(pre, post, 0, n - 1, 0, n - 1);
     }
 };
 
