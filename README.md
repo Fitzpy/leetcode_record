61a1ca34a49fefbef561f894a8de199e60a02572
fitzpeng <821397569@qq.com>
2020-10-20 00:15:15 +0800

    剑指 Offer 11. 旋转数组的最小数字

--- README.md
+++ README.md
@@ -2492,3 +2492,20 @@ public:
     }
 };
 ```
+
+## 剑指 Offer 11. 旋转数组的最小数字
+```
+class Solution {
+public:
+    int minArray(vector<int> &numbers) {
+        int low = 0, high = numbers.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (numbers[mid] > numbers[high]) low = mid + 1;
+            else if (numbers[mid] < numbers[high]) high = mid;
+            else high--;
+        }
+        return numbers[low];
+    }
+};
+```
--- main.cpp
+++ main.cpp
@@ -2,57 +2,21 @@
 
 using namespace std;
 
-int a[] = {1, 2, 3, 5, 6, 7, 9, 11};
-int L[105], R[105];
-int n = 8;
-
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int> &nums) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++) nums.push_back(nums[i]);
-        n *= 2;
-        vector<int> L(n + 1, 0);
-        stack<int> st;
-        for (int i = 0; i < n; i++) {
-            if (st.empty()) st.push(i);
-            else {
-                while (!st.empty() && nums[st.top()] < nums[i]) {
-                    L[st.top()] = nums[i];
-                    st.pop();
-                }
-                st.push(i);
-            }
+    int minArray(vector<int> &numbers) {
+        int low = 0, high = numbers.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (numbers[mid] > numbers[high]) low = mid + 1;
+            else if (numbers[mid] < numbers[high]) high = mid;
+            else high--;
         }
-        while (!st.empty()) {
-            L[st.top()] = -1;
-            st.pop();
-        }
-        vector<int> ret;
-        for (int i = 0; i < n / 2; i++) ret.push_back(L[i]);
-        return ret;
+        return numbers[low];
     }
 };
 
 int main() {
-    stack<int> st;
-    for (int i = 0; i < n; i++) {
-        if (st.empty()) st.push(i);
-        else {
-            while (!st.empty() && a[st.top()] <= a[i]) {
-                L[st.top()] = a[i];
-                st.pop();
-            }
-            st.push(i);
-        }
-    }
-    while (!st.empty()) {
-        L[st.top()] = 999999;
-        st.pop();
-    }
-    for (int i = 0; i < n; i++) {
-        printf("%d ", L[i]);
-    }
-    printf("\n");
+
     return 0;
 }
