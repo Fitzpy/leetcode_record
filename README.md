ebb64085283b5b343ce3bef0692ba5b68afd9e6d
fitzpeng <821397569@qq.com>
2020-10-20 11:13:47 +0800

    670. 最大交换

--- README.md
+++ README.md
@@ -2527,3 +2527,38 @@ public:
     }
 };
 ```
+## 670. 最大交换
+```
+class Solution {
+public:
+    int maximumSwap(int num) {
+        int a[15], len = 0;
+        int ret = num;
+        while (num) {
+            a[len++] = num % 10;
+            num /= 10;
+        }
+        reverse(a, a + len);
+        int pos = 1;
+        for (; pos < len; pos++) if (a[pos] > a[pos - 1]) break;
+        if (pos == len) return ret;
+        int maxx = a[pos], maxPos = pos;
+        for (int i = pos; i < len; i++) {
+            if (a[i] >= maxx) {
+                maxx = a[i];
+                maxPos = i;
+            }
+        }
+        for (int i = 0; i < pos; i++) {
+            if (a[i] < maxx) {
+                swap(a[i], a[maxPos]);
+                break;
+            }
+        }
+        ret = 0;
+        for (int i = 0; i < len; i++)
+            ret = ret * 10 + a[i];
+        return ret;
+    }
+};
+```
--- main.cpp
+++ main.cpp
@@ -2,23 +2,40 @@
 
 using namespace std;
 
+
 class Solution {
 public:
-    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
-        int sum = 0, cur = 0, pos = -1;
-        for (int i = 0; i < gas.size(); i++) {
-            sum += gas[i] - cost[i];
-            cur += gas[i] - cost[i];
-            if (cur < 0) {
-                pos = i + 1;
-                cur = 0;
+    int maximumSwap(int num) {
+        int a[15], len = 0;
+        int ret = num;
+        while (num) {
+            a[len++] = num % 10;
+            num /= 10;
+        }
+        reverse(a, a + len);
+        int pos = 1;
+        for (; pos < len; pos++) if (a[pos] > a[pos - 1]) break;
+        if (pos == len) return ret;
+        int maxx = a[pos], maxPos = pos;
+        for (int i = pos; i < len; i++) {
+            if (a[i] >= maxx) {
+                maxx = a[i];
+                maxPos = i;
             }
         }
-        return sum >= 0 ? pos : -1;
+        for (int i = 0; i < pos; i++) {
+            if (a[i] < maxx) {
+                swap(a[i], a[maxPos]);
+                break;
+            }
+        }
+        ret = 0;
+        for (int i = 0; i < len; i++)
+            ret = ret * 10 + a[i];
+        return ret;
     }
 };
 
 int main() {
-
     return 0;
 }
