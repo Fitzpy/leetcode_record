09c994a8c45c7e184143db5316a7a0f6e5fb83eb
fitzpeng <821397569@qq.com>
2020-10-21 15:15:39 +0800

    ## 719. 找出第 k 小的距离对

--- README.md
+++ README.md
@@ -2708,3 +2708,32 @@ public:
 };
 
 ```
+## 719. 找出第 k 小的距离对
+```
+class Solution {
+public:
+    int check(vector<int> &nums, int mid) {
+        int L = 0, R, ret = 0;
+        for (R = 0; R < nums.size(); R++) {
+            while (nums[R] - nums[L] > mid) L++;
+            ret += R - L;
+        }
+        return ret;
+    }
+
+    int smallestDistancePair(vector<int> &nums, int k) {
+        sort(nums.begin(), nums.end());
+        int low = 0, high = nums.back(), ans;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (check(nums, mid) >= k) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+```
--- main.cpp
+++ main.cpp
@@ -1,43 +1,36 @@
 
-// atomic::compare_exchange_weak example:
-#include <iostream>       // std::cout
-#include <atomic>         // std::atomic
-#include <thread>         // std::thread
-#include <vector>         // std::vector
+#include <bits/stdc++.h>
 
-// a simple global linked list:
-struct Node {
-    int value;
-    Node *next;
+using namespace std;
+
+class Solution {
+public:
+    int check(vector<int> &nums, int mid) {
+        int L = 0, R, ret = 0;
+        for (R = 0; R < nums.size(); R++) {
+            while (nums[R] - nums[L] > mid) L++;
+            ret += R - L;
+        }
+        return ret;
+    }
+
+    int smallestDistancePair(vector<int> &nums, int k) {
+        sort(nums.begin(), nums.end());
+        int low = 0, high = nums.back(), ans;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (check(nums, mid) > k) {
+                high = mid - 1;
+            } else {
+                ans = mid;
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
 };
-std::atomic<Node *> list_head(nullptr);
-
-void append(int val) {     // append an element to the list
-    Node *oldHead = list_head;
-    Node *newNode = new Node{val, oldHead};
-
-    // what follows is equivalent to: list_head = newNode, but in a thread-safe way:
-    while (!list_head.compare_exchange_weak(oldHead, newNode))
-        newNode->next = oldHead;
-}
 
 int main() {
-    // spawn 10 threads to fill the linked list:
-    std::vector<std::thread> threads;
-    for (int i = 0; i < 10; ++i) threads.push_back(std::thread(append, i));
-    for (auto &th : threads) th.join();
-
-    // print contents:
-    for (Node *it = list_head; it != nullptr; it = it->next)
-        std::cout << ' ' << it->value;
-    std::cout << '\n';
-
-    // cleanup:
-    Node *it;
-    while (it = list_head) {
-        list_head = it->next;
-        delete it;
-    }
 
     return 0;
 }
