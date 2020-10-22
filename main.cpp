dc296d3232dfadfc8a0f8d0d60bc30506d46f1cd
fitzpeng <821397569@qq.com>
2020-10-22 17:32:50 +0800

    ## 网易互娱二面

--- main.cpp
+++ main.cpp
@@ -1,52 +1,85 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-class RandomizedSet {
-public:
-    /** Initialize your data structure here. */
-    RandomizedSet() {
-        v.clear();
-        mp.clear();
+int a[] = {1, -2, 1, 0, 3};
+
+void quickSort(int L, int R) {
+    if (L > R) return;
+    int i = L, j = R, val = a[L];
+    while (i < j) {
+        while (i < j && a[j] >= val) j--;
+        if (i < j) a[i++] = a[j];
+        while (i < j && a[i] <= val) i++;
+        if (i < j) a[j--] = a[i];
     }
+    a[i] = val;
+    quickSort(L, i - 1);
+    quickSort(i + 1, R);
+}
 
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    bool insert(int val) {
-        if (mp.count(val)) return false;
-        v.push_back(val);
-        mp[val] = v.size() - 1;
-        return true;
+void merge(int L1, int R1, int L2, int R2) {
+    if (L1 > R1 || L2 > R2) return;
+    int i = L1, j = L2, pos = 0;
+    int temp[R2 - L1 + 1];
+    while (i <= R1 && j <= R2) {
+        if (a[i] < a[j]) temp[pos++] = a[i++];
+        else temp[pos++] = a[j++];
     }
-
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    bool remove(int val) {
-        if (!mp.count(val)) return false;
-        int idx = mp[val];
-        mp[v.back()] = idx;
-        swap(v[idx], v.back());
-        v.pop_back();
-        return true;
+    while (i <= R1) temp[pos++] = a[i++];
+    while (j <= R2) temp[pos++] = a[j++];
+    for (i = L1; i <= R2; i++) {
+        a[i] = temp[i - L1];
     }
+}
 
-    /** Get a random element from the set. */
-    int getRandom() {
-        return v[rand() % (v.size())];
+void mergeSort(int L, int R) {
+    if (L >= R) return;
+    int mid = (L + R) / 2;
+    mergeSort(L, mid);
+    mergeSort(mid + 1, R);
+    merge(L, mid, mid + 1, R);
+}
+
+int top, heap[10010];
+
+void push(int val) {
+    heap[++top] = val;
+    int temp = top;
+    while (temp > 1) {
+        int root = temp / 2;
+        if (heap[root] >= val) {
+            swap(heap[root], heap[temp]);
+            temp = root;
+        } else break;
     }
+}
 
-private:
-    vector<int> v;
-    unordered_map<int, int> mp;
-};
+int pop() {
+    int ret = heap[1];
+    heap[1] = heap[top];
+    int root = 1;
+    while (2 * root < top) {
+        int L = root * 2, R = root * 2 + 1;
+        if (R >= top || heap[L] <= heap[R]) {
+            if (heap[L] <= heap[root]) {
+                swap(heap[L], heap[root]);
+                root = L;
+            } else break;
+        } else {
+            if (heap[R] <= heap[root]) {
+                swap(heap[R], heap[root]);
+                root = R;
+            } else break;
+        }
+    }
+    top--;
+    return ret;
+}
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
 int main() {
-
+    for (int i = 0; i < 5; i++) push(a[i]);
+    for (int i = 0; i < 5; i++) printf("%d ", pop());
+    printf("\n");
     return 0;
 }
