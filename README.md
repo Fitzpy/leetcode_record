c6a9d5839426d6ab84b3b63e4400322511cf9fcf
fitzpeng <821397569@qq.com>
2020-11-05 22:41:40 +0800

    ## 无锁队列(循环数组)

--- README.md
+++ README.md
@@ -853,7 +853,7 @@ String::String(const String &s) {
     }
 }
 
-String &String::operator=(const String &s) {
+String &String::operator=(const Strin~~~~g &s) {
     //检查是否是自赋值
     if (this == &s) {
         return *this;
@@ -2890,4 +2890,92 @@ public:
         return ret;
     }
 };
+```
+## 无锁队列(循环数组)
+```
+#include<bits/stdc++.h>
+
+using namespace std;
+const int maxn = 1e5 + 7;
+
+class lockFreeQueue {
+public:
+    lockFreeQueue() {
+        writeIndex = readIndex = maxReadIndex = 0;
+    }
+
+    bool push(int val) {
+        int currentReadIndex, currentWriteIndex, nextWriteIndex;
+        do {
+            currentWriteIndex = writeIndex;
+            currentReadIndex = readIndex;
+            nextWriteIndex = (writeIndex + 1) % maxn;
+            if (nextWriteIndex == currentReadIndex) {
+                return false;//full
+            }
+        } while (!__sync_bool_compare_and_swap(&writeIndex, currentWriteIndex, nextWriteIndex));
+        data[currentWriteIndex] = val;
+        while (!__sync_bool_compare_and_swap(&maxReadIndex, currentWriteIndex, nextWriteIndex)) {
+            std::this_thread::yield();
+        }
+        return true;
+    }
+
+    bool pop(int &ret) {
+        int currentMaxReadIndex;
+        int currentReadIndex;
+        int nextReadIndex;
+        do {
+            currentReadIndex = readIndex;
+            currentMaxReadIndex = maxReadIndex;
+            nextReadIndex = (currentReadIndex + 1) % maxn;
+            if (currentReadIndex == currentMaxReadIndex) {
+                //队列为空或生产者线程在队列中分配了空间，但是等待将数据提交到其中
+                return false;
+            }
+            ret = data[currentReadIndex];
+        } while (!__sync_bool_compare_and_swap(&readIndex, currentReadIndex, nextReadIndex));
+        return true;
+    }
+
+private:
+    int writeIndex, readIndex, maxReadIndex;
+    int data[maxn];
+};
+
+lockFreeQueue que;
+
+void func1() {
+    for (int i = 0; i < 10; i++) {
+        que.push(i);
+    }
+}
+
+void func2() {
+    for (int i = 30; i < 40; i++) {
+        que.push(i);
+    }
+}
+
+void func3() {
+    for (int i = 0; i < 10; i++) {
+        int ret;
+        que.pop(ret);
+        printf("%d ",ret);
+    }
+    printf("\n");
+}
+
+int main() {
+    std::thread t1(func1);
+    std::thread t2(func2);
+    std::thread t3(func3);
+    std::thread t4(func3);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    return 0;
+}
+
 ```
--- main.cpp
+++ main.cpp
@@ -1,153 +1,84 @@
-#include <iostream>
-#include <thread>
-#include <atomic>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
+const int maxn = 1e5 + 7;
 
-template<class T>
-class LinkNode {
+class lockFreeQueue {
 public:
-    T data;
-    LinkNode<T> *next;
-};
-
-template<class T>
-class LockFreeQueue {
-public:
-    LockFreeQueue<T>() : head_(nullptr), tail_(nullptr), len(0) {
-        LinkNode<T> *now = new LinkNode<T>;
-        now->next = nullptr;
-        head_ = tail_ = now;
+    lockFreeQueue() {
+        writeIndex = readIndex = maxReadIndex = 0;
     }
 
-    void push(const T &data);
+    bool push(int val) {
+        int currentReadIndex, currentWriteIndex, nextWriteIndex;
+        do {
+            currentWriteIndex = writeIndex;
+            currentReadIndex = readIndex;
+            nextWriteIndex = (writeIndex + 1) % maxn;
+            if (nextWriteIndex == currentReadIndex) {
+                return false;//full
+            }
+        } while (!__sync_bool_compare_and_swap(&writeIndex, currentWriteIndex, nextWriteIndex));
+        data[currentWriteIndex] = val;
+        while (!__sync_bool_compare_and_swap(&maxReadIndex, currentWriteIndex, nextWriteIndex)) {
+            std::this_thread::yield();
+        }
+        return true;
+    }
 
-    T pop_front();
-
-    bool empty();
+    bool pop(int &ret) {
+        int currentMaxReadIndex;
+        int currentReadIndex;
+        int nextReadIndex;
+        do {
+            currentReadIndex = readIndex;
+            currentMaxReadIndex = maxReadIndex;
+            nextReadIndex = (currentReadIndex + 1) % maxn;
+            if (currentReadIndex == currentMaxReadIndex) {
+                //队列为空或生产者线程在队列中分配了空间，但是等待将数据提交到其中
+                return false;
+            }
+            ret = data[currentReadIndex];
+        } while (!__sync_bool_compare_and_swap(&readIndex, currentReadIndex, nextReadIndex));
+        return true;
+    }
 
 private:
-    LinkNode<T> *head_;
-    LinkNode<T> *tail_;
-    std::atomic_int len;
+    int writeIndex, readIndex, maxReadIndex;
+    int data[maxn];
 };
 
-template<class T>
-void LockFreeQueue<T>::push(const T &data) {
-    LinkNode<T> *now = new LinkNode<T>;
-    now->data = data;
-    now->next = nullptr;
-    LinkNode<T> *p1, *nxt;
-    while (1) {
-        p1 = tail_;
-        nxt = tail_->next;
-        if (p1 != tail_) continue;
-        if (nxt != nullptr) {
-            __sync_bool_compare_and_swap(&tail_, p1, nxt);
-            continue;
-        }
-        if (__sync_bool_compare_and_swap(&tail_->next, nxt, now) == 1) {
-            break;
-        }
-    }
-    __sync_bool_compare_and_swap(&tail_, p1, now);
-    len++;
-}
+lockFreeQueue que;
 
-template<class T>
-T LockFreeQueue<T>::pop_front() {
-    LinkNode<T> *p1;
-    do {
-        p1 = head_->next;
-        if (p1 == nullptr) {
-            return -999999;
-        }
-    } while (__sync_bool_compare_and_swap(&head_->next, p1, p1->next) != true);
-    len--;
-    return p1->data;
-}
-
-template<class T>
-bool LockFreeQueue<T>::empty() {
-    return len == 0;
-}
-
-LockFreeQueue<int> myQueue;
-
-void func1(void) {
+void func1() {
     for (int i = 0; i < 10; i++) {
-        myQueue.push(i);
+        que.push(i);
     }
 }
 
-void func2(void) {
+void func2() {
     for (int i = 30; i < 40; i++) {
-        myQueue.push(i);
+        que.push(i);
     }
 }
 
-void func3(void) {
+void func3() {
     for (int i = 0; i < 10; i++) {
-        printf("%d ", myQueue.pop_front());
+        int ret;
+        que.pop(ret);
+        printf("%d ",ret);
     }
+    printf("\n");
 }
 
-class Solution {
-public:
-    struct node {
-        int val;
-        node *lson, *rson;
-
-        node(int val) : val(val), lson(NULL), rson(NULL) {}
-    };
-
-    unordered_map<int, int> mp;
-
-    node *build(vector<int> &inorder, vector<int> &postorder, int L1, int R1, int L2, int R2) {
-        if (L1 > R1 || L2 > R2) return NULL;
-        int pos = mp[postorder[R2]];
-        if (pos > R1 || pos < L1) return NULL;
-        node *head = new node(postorder[R2]);
-        head->lson = build(inorder, postorder, L1, pos - 1, L2, L2 + pos - L1 - 1);
-        head->rson = build(inorder, postorder, pos + 1, R1, L2 + pos - L1, R2 - 1);
-        return head;
-    }
-
-    bool check(node *head) {
-        if (head == NULL) return true;
-        node *cur = head;
-        if (cur->lson) {
-            if (cur->lson->val > head->val) return false;
-        }
-        if (cur->rson) {
-            if (cur->rson->val < head->val) return false;
-        }
-        bool ret = (check(cur->lson) && check(cur->rson));
-        return ret;
-    }
-
-    int getNum(node *head) {
-        if (head == NULL) return 0;
-        return getNum(head->lson) + 1 + getNum(head->rson);
-    }
-
-    bool verifyPostorder(vector<int> &postorder) {
-        vector<int> inorder = postorder;
-        sort(inorder.begin(), inorder.end());
-        mp.clear();
-        int n = postorder.size();
-        for (int i = 0; i < n; i++) {
-            mp[inorder[i]] = i;
-        }
-        node *head = build(inorder, postorder, 0, n - 1, 0, n - 1);
-        int num = getNum(head);
-        if (num != n) return false;
-        return check(head);
-    }
-};
-
-int main(int argc, char **argv) {
-
+int main() {
+    std::thread t1(func1);
+    std::thread t2(func2);
+    std::thread t3(func3);
+    std::thread t4(func3);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
     return 0;
 }
